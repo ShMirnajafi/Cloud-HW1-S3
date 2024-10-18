32668c00a1a8062190ff904c1f780f4c7031e003
mirnajafizade <mirnajafizade@mahsan.co>
2024-10-18 12:14:48 +0330

    final version 1.0.0

--- src/app/api/caption-to-image/route.js
+++ src/app/api/caption-to-image/route.js
@@ -1,3 +1,29 @@
+import { NextResponse } from 'next/server';
+import { processRequests } from '@/lib/serviceThree';
+
+let intervalId = null;
+
 export async function GET(request) {
-    return new Response('Service Three is running!');
+    if (intervalId !== null) {
+        return NextResponse.json({ message: 'Process is already running periodically.' });
+    }
+
+    intervalId = setInterval(() => {
+        console.log('Running periodic task to process requests...');
+        processRequests()
+            .then(() => console.log('Processed requests successfully'))
+            .catch((err) => console.error('Error processing requests:', err));
+    }, 10000);
+
+    return NextResponse.json({ message: 'Periodic processing started. It will run every 10 seconds.' });
+}
+
+export async function DELETE(request) {
+    if (intervalId !== null) {
+        clearInterval(intervalId);
+        intervalId = null;
+        return NextResponse.json({ message: 'Periodic processing stopped.' });
+    } else {
+        return NextResponse.json({ message: 'No periodic process was running.' });
+    }
 }
